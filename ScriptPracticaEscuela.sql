b0fe6205d92347787033dcd83ac5e7e695d783f9
Adrian Camus <adrianedgardocamus@gmail.com>
2022-07-11 13:42:59 -0300

    Create ScriptPracticaEscuela.sql

---
+++ ScriptPracticaEscuela.sql
@@ -0,0 +1,22 @@
+##INSERT por cada tabla
+## 1.-   Insertar localidades
+## 2.-   Insertar alumnos
+## 3.-   Insertar carreras
+## 4.-   Insertar Materias
+## 5.-   Inscribir alumnos a materias
+
+## 1
+INSERT into Localidad (idLocalidad, nombre) values (1,'Mendoza');
+INSERT into Localidad (idLocalidad, nombre) values (2,'Maipu');
+INSERT into Localidad (idLocalidad, nombre) values (3,'San Martin');
+INSERT into Localidad (idLocalidad, nombre) values (4,'Cordoba');
+INSERT into Localidad (idLocalidad, nombre) values (5,'Rio Cuarto');
+
+## 2 
+INSERT into Alumno (Legajo, Nombre, Apellido, Localidad, Telefono) values (27459881, 'Test', 'User', '3','2612493358');
+INSERT into Alumno (Legajo, Nombre, Apellido, Localidad, Telefono) values (37449881, 'Elba', 'Gallo', '4','3512493358');
+INSERT into Alumno (Legajo, Nombre, Apellido, Localidad, Telefono) values (29455181, 'Susana', 'Oria', '2','2612493358');
+INSERT into Alumno (Legajo, Nombre, Apellido, Localidad, Telefono) values (32449889, 'Armando', 'Paredes', '5','3512484358');
+INSERT into Alumno (Legajo, Nombre, Apellido, Localidad, Telefono) values (40587214, 'Sevelinda', 'Parada', '1','26124974710');
+
+## 3 
